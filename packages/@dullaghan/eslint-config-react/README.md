1b3943347736cb1afad17dd076eefa2df2d276e7
Brian George <bgeorge@horizontal.com>
2022-04-21 14:48:50 -0500

    Added eslint-config-react package.

---
+++ packages/@dullaghan/eslint-config-react/README.md
@@ -0,0 +1,19 @@
+# `@dullaghan/eslint-config-react`
+
+Shared eslint config for React based projects
+
+## Usage
+
+Install this package as a dev dependency
+
+```
+yarn add -D @dullaghan/eslint-config-react
+```
+
+Add an `.eslintrc.json` file to the root of your project with the following
+
+```json
+{
+  "extends": ["@dullaghan/eslint-config-react"]
+}
+```
---
+++ packages/@dullaghan/eslint-config-react/index.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'import/no-unresolved': 'error',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/require-default-props': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+  },
+};
